97606aa6e5af8c9a6a3e947d9cb376c5ffa46ddf
BEIDI DINA SAMUEL <77502731+samglish@users.noreply.github.com>
2024-04-10 12:01:11 +0200

    upload

---
+++ main.py
@@ -0,0 +1,24 @@
+phrase = input("Entrez une phrase :")
+
+sms_dictionnary = {' ': ' ', 'bonjour': 'bjr', 'salut': 'slt'}  # completer le dictionnaire
+ 
+ 
+def sms_translator():
+ 
+    sms_message = ""
+    for x in phrase:
+        try: # on éssait de traduire
+            sms_message += sms_dictionnary[x]
+            sms_message += " "
+        except KeyError:# sinon on restitue le mot tel qu'il est.
+            sms_message += phrase[phrase.index(x)]
+            sms_message += " "
+    print(sms_message)
+ 
+ 
+phrase += ""
+ 
+phrase = [mot for mot in phrase.split(" ")]
+ 
+ 
+sms_translator()
