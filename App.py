4e26a5502f5d0e6ebb4500c1f3f52902ce877dd4
BEIDI DINA SAMUEL <77502731+samglish@users.noreply.github.com>
2024-04-26 22:11:40 +0200

    update

---
+++ App.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from tkinter import ttk
+class App(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+
+# create the application
+myapp = App()
+#
+# here are method calls to the window manager class
+#
+myapp.master.title("SamGlish")
+myapp.master.maxsize(500, 500)
+myapp.master.minsize(500, 500)
+myapp.master.geometry("500x500")
+myapp.master.configure(bg="lightblue")
+# start the program
+Frame=ttk.Frame(myapp, padding=0)
+Frame.grid()
+ttk.Label(Frame, text="Wadjonautes translator", font=("Arial", 15),background="lightblue",padding=0).grid(column=0, row=0)
+ttk.Label(Frame, text="FR to FB",background="lightblue",padding=0).grid(column=0, row=1)
+
+ttk.Entry(Frame,show=True,background="white").grid(column=0, row=8)
+myapp.mainloop()
