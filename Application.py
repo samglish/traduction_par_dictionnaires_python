3c795ac9f4e87d9e8f8a3145dbd76daca10ac9b5
BEIDI DINA SAMUEL <77502731+samglish@users.noreply.github.com>
2024-04-28 03:33:45 +0200

    update

--- Application.py
+++ Application.py
@@ -1,15 +1,16 @@
 import re
+from tkinter import *
 import tkinter as tk
 gui = tk.Tk()
 gui.geometry("500x500")
 gui.title("Samglish Translator")
 gui.configure(background="lightblue")
-def Translate(phrase):
+def Translate(phrase:StringVar):
     motsFR = ["bonjour", "salut"]
     motsFB = ["bjr", "slt"]
     for i in range(len(motsFR)):
         phrase = re.sub(motsFR[i], motsFB[i], phrase)
-    return phrase
+    print(phrase)
 
 myLabel=tk.Label(gui, text="Wadjonautes translator", font=("Arial", 15),background="lightblue")
 myLabel.pack()
@@ -17,12 +18,13 @@ myLabel1=tk.Label(gui, text="FR to FB",background="lightblue")
 myLabel1.pack()
 myEntry = tk.Entry(gui, width=40)
 myEntry.pack(pady=20)
+
 reponse=tk.StringVar()
-reponse=Translate(myEntry.get)
+reponse="resultats"
+
 btn = tk.Button(gui, height=1, width=10, text="Traduire", command=Translate(reponse))
 btn.pack()
-
-Affiche=tk.Label(gui, textvariable=reponse,background="lightblue")
-Affiche.pack()
+Affiche=tk.Label(gui, text=reponse,relief=RAISED, width=10)
+Affiche.pack(pady=20)
 
 gui.mainloop()
